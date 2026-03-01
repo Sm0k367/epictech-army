a5d67af91aae80b3016a86d243dabf3803e67479
Sm0k367 <139561920+Sm0k367@users.noreply.github.com>
2026-03-01 10:46:08 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+```js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+reactStrictMode: true,
+experimental: {
+appDir: true
+},
+images: {
+domains: [],
+},
+}
+
+module.exports = nextConfig;
+```
